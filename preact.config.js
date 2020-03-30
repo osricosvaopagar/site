c3b180ad14676462ba281ebf4957aeb616ab5f1a
Pedro Andrade <pdandradeb@gmail.com>
2020-03-30 14:43:23 -0300

    Disabling purgecss to fix styles

--- preact.config.js
+++ preact.config.js
@@ -1,10 +1,10 @@
 module.exports = (config, env, helpers, params) => {
-  const purgecss = require('@fullhuman/postcss-purgecss')({
-    // Specify the paths to all of the template files in your project
-    content: ['./src/**/*.js'],
-    // Include any special characters you're using in this regular expression
-    defaultExtractor: content => content.match(params.regex) || [],
-  });
+  // const purgecss = require('@fullhuman/postcss-purgecss')({
+  //   // Specify the paths to all of the template files in your project
+  //   content: ['./src/**/*.js'],
+  //   // Include any special characters you're using in this regular expression
+  //   defaultExtractor: content => content.match(params.regex) || [],
+  // });
   const postCssLoaders = helpers.getLoadersByName(config, 'postcss-loader');
   postCssLoaders.forEach(({ loader }) => {
     const plugins = loader.options.plugins;
@@ -12,7 +12,7 @@ module.exports = (config, env, helpers, params) => {
     plugins.unshift(require('tailwindcss'));
     // Add PurgeCSS only in production.
     if (env.production) {
-      plugins.push(purgecss);
+      // plugins.push(purgecss);
     }
   });
   return config;
