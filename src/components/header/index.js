82dd2e2dc8f32a25e361ba71a33e666d8a68c5fc
Pedro Andrade <pdandradeb@gmail.com>
2020-03-30 11:26:09 -0300

    Initial version

---
+++ src/components/header/index.js
@@ -0,0 +1,9 @@
+import logo from '../../assets/logo.png';
+
+const Header = () => (
+  <div class="">
+    <img src={logo} />
+  </div>
+);
+
+export default Header;
---
+++ src/routes/home/index.js
@@ -0,0 +1,35 @@
+import { h } from 'preact';
+import style from './style';
+
+const Home = () => (
+	<div class="">
+		<article>
+        <h1 class="py-10">Se os <span class="font-bold underline">0,001%</span> de brasileiros milionários <span class="font-bold underline">pagassem impostos</span> como em qualquer país desenvolvido, nós poderíamos ter o que nunca tivemos: segurança, saneamento básico, educação e saúde de qualidade.</h1>
+        <p>Não precisamos de uma pandemia para saber que <strong>o Brasil tem carência de quase tudo</strong>. Não precisamos uma pandemia para sabermos que <strong>a principal preocupação da elite brasileira sua riqueza</strong> e não com as pessoas.</p>
+        <p>Neste momento, onde boa parte da população <strong>não sabe se vai conseguir ser atendido ou pagar por suas necessidades básicas</strong>, os ricos estão se organizando para evitar prejuízos ou até lucrar com a crise.</p>
+        <p>Precisamos nos organizar para reparar uma injustiça antiga no Brasil: <strong>chegou a hora de os ricos pagarem o que devem.</strong></p>
+        <h1 class="py-6">O que queremos: aumento da alíquota de IR para milionários</h1>
+        <div class="pb-6">
+          <h3><a href="https://exame.abril.com.br/economia/brasil-esta-entre-paises-que-menos-cobram-impostos-sobre-renda-no-mundo/">"Brasil está entre países que menos cobram impostos sobre renda no mundo"</a></h3>
+          <a class="text-xs" href="https://exame.abril.com.br/economia/brasil-esta-entre-paises-que-menos-cobram-impostos-sobre-renda-no-mundo/">https://exame.abril.com.br/economia/brasil-esta-entre-paises-que-menos-cobram-impostos-sobre-renda-no-mundo/</a>
+        </div>
+        <p>Países desenvolvidos, que já contam com ótima infraestrutura e precisam de muito menos recursos do que nós, taxam cerca de 50% da renda.</p>
+        <p><strong>Enquanto isso, no Brasil, a alíquota de IR para milionários e bilionários é de inacreditáveis 27,5%!</strong></p>
+        <p>Após as guerras mundiais, <strong>países como Estados Unidos e Reino Unido chegaram a cobrar 90% de alíquota de IR para os mais ricos.</strong> Quem acha que nossa situação não é grave para merecer medida semelhante?</p>
+        <p><strong>A elite egoísta não vai fazer isso voluntariamente!</strong></p>
+        <p>Ela prefere morar em condomínios fechados e blindar os carros do que pagar mais impostos. Ela quer nos convencer que votando nos candidatos "deles" a corrupção vai acabar e todos os problemas do país deixarão de existir da noite pro dia.</p>
+        <p><strong>Não temos tempo!</strong></p>
+        <p>Precisamos de dinheiro para diminuir a impunidade e corrupção. Precisamos de dinheiro para melhorar nossa educação. Precisamos de dinheiro para qualificar nossos profissionais. Precisamos de dinheiro para que as pessoas tenham saneamento básico!</p>
+        <h1 class="py-6">Essa é nossa hora de agir!</h1>
+        <ol class="pb-6">
+          <li>Assine a petição para aumentar as alíquotas de IR para os milionários e bilionários brasileiros</li>
+          <li>Divulgue esse movimento no WhatsApp e nas redes sociais.</li>
+          <li>Entre em contato com deputados e senadores para que essa discussão aconteça o mais rápido possível</li>
+        </ol>
+        <p>Envie ideias, textos e memes para assim&zwj;@&zwj;osricosvaopagar.com‍</p>
+        <p>Se você tem outras sugestões de como fazer os ricos pagarem a conta, entra em contato com a gente.</p>
+      </article>
+	</div>
+);
+
+export default Home;
